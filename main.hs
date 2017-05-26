6f4dc36d5ef03db7f858df7374231240284502c6
llnc <luana.n.costa@hotmail.com>
2017-05-26 00:59:22 +0000

    base do projeto, implementacao da tabela Paciente

---
+++ main.hs
@@ -0,0 +1,50 @@
+import Yesod
+import Database.Persist.Postgresql
+import Data.Text
+import Control.Monad.Logger (runStdoutLoggingT)
+
+
+             
+data App = App{connPool :: ConnectionPool}
+
+instance Yesod App 
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
+
+Paciente 
+    nome Text
+    idade Int
+    cpf Text
+   deriving Show
+  
+
+|]
+
+
+mkYesod "App" [parseRoutes|
+/ HomeR GET
+
+|]
+
+getHomeR :: Handler ()
+getHomeR = undefined
+
+
+instance YesodPersist App where
+   type YesodPersistBackend App = SqlBackend
+   runDB f = do
+       master <- getYesod
+       let pool = connPool master
+       runSqlPool f pool
+------------------------------------------------------
+
+
+
+connStr = "dbname=dbs10j40oj2r50 host=ec2-54-225-95-99.compute-1.amazonaws.com user=ywzfzssfskwcju password=QOoBT4kER9KgAod70niCY3T6tB port=5432"
+
+main::IO()
+main = runStdoutLoggingT $ withPostgresqlPool connStr 10 $ \pool -> liftIO $ do 
+       runSqlPersistMPool (runMigration migrateAll) pool
+       warp 8080 (App pool)
+       
+       
